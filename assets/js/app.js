0d19d5798baa9cd35decc27a6798d7101ee18dbc
David Sulc <davidsulc@gmail.com>
2013-07-20 21:24:29 +0200

    load the dialog region dependency

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,33 @@
-var ContactManager = new Marionette.Application();
+define(["marionette", "apps/config/marionette/regions/dialog"], function(Marionette){
+  var ContactManager = new Marionette.Application();
 
-ContactManager.addRegions({
-  headerRegion: "#header-region",
-  mainRegion: "#main-region",
-  dialogRegion: Marionette.Region.Dialog.extend({
-    el: "#dialog-region"
-  })
-});
+  ContactManager.addRegions({
+    headerRegion: "#header-region",
+    mainRegion: "#main-region",
+    dialogRegion: Marionette.Region.Dialog.extend({
+      el: "#dialog-region"
+    })
+  });
 
-ContactManager.navigate = function(route,  options){
-  options || (options = {});
-  Backbone.history.navigate(route, options);
-};
+  ContactManager.navigate = function(route,  options){
+    options || (options = {});
+    Backbone.history.navigate(route, options);
+  };
 
-ContactManager.getCurrentRoute = function(){
-  return Backbone.history.fragment
-};
+  ContactManager.getCurrentRoute = function(){
+    return Backbone.history.fragment
+  };
 
-ContactManager.on("initialize:after", function(){
-  if(Backbone.history){
-    Backbone.history.start();
+  ContactManager.on("initialize:after", function(){
+    console.log("Contact Manager has started");
+    if(Backbone.history){
+      Backbone.history.start();
 
-    if(this.getCurrentRoute() === ""){
-      ContactManager.trigger("contacts:list");
+      if(this.getCurrentRoute() === ""){
+        ContactManager.trigger("contacts:list");
+      }
     }
-  }
+  });
+
+  return ContactManager;
 });
--- assets/js/apps/config/marionette/regions/dialog.js
+++ assets/js/apps/config/marionette/regions/dialog.js
@@ -1,21 +1,25 @@
-Marionette.Region.Dialog = Marionette.Region.extend({
-  onShow: function(view){
-    this.listenTo(view, "dialog:close", this.closeDialog);
+define(["marionette", "jquery-ui"], function(Marionette){
+  Marionette.Region.Dialog = Marionette.Region.extend({
+    onShow: function(view){
+      this.listenTo(view, "dialog:close", this.closeDialog);
 
-    var self = this;
-    this.$el.dialog({
-      modal: true,
-      title: view.title,
-      width: "auto",
-      close: function(e, ui){
-        self.closeDialog();
-      }
-    });
-  },
+      var self = this;
+      this.$el.dialog({
+        modal: true,
+        title: view.title,
+        width: "auto",
+        close: function(e, ui){
+          self.closeDialog();
+        }
+      });
+    },
 
-  closeDialog: function(){
-    this.stopListening();
-    this.close();
-    this.$el.dialog("destroy");
-  }
+    closeDialog: function(){
+      this.stopListening();
+      this.close();
+      this.$el.dialog("destroy");
+    }
+  });
+
+  return Marionette.Region.Dialog;
 });
