85d3d3dd40d0cebd858224c3903a12d6998d668d
David Sulc <davidsulc@gmail.com>
2013-10-29 20:53:36 +0100

    using almond to load the optimized application

--- assets/js/build.js
+++ assets/js/build.js
@@ -1,6 +1,8 @@
 ({
     baseUrl: ".",
-    name: "require_main",
+    name: "vendor/almond",
+    include: "require_main",
     mainConfigFile: "require_main.js",
-    out: "require_main.built.js"
+    out: "require_main.built.js",
+    findNestedDependencies: true
 })
