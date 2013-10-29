57afa9b845899fba124279adcdef9f5af936bf0b
David Sulc <davidsulc@gmail.com>
2013-10-29 20:38:35 +0100

    edit contacts

--- assets/js/apps/contacts/edit/edit_controller.js
+++ assets/js/apps/contacts/edit/edit_controller.js
@@ -1,36 +1,42 @@
-ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager, Backbone, Marionette, $, _){
-  Edit.Controller = {
-    editContact: function(id){
-      var loadingView = new ContactManager.Common.Views.Loading({
-        title: "Artificial Loading Delay",
-        message: "Data loading is delayed to demonstrate using a loading view."
-      });
-      ContactManager.mainRegion.show(loadingView);
-
-      var fetchingContact = ContactManager.request("contact:entity", id);
-      $.when(fetchingContact).done(function(contact){
-        var view;
-        if(contact !== undefined){
-          view = new Edit.Contact({
-            model: contact,
-            generateTitle: true
+define(["app", "apps/contacts/edit/edit_view"], function(ContactManager, View){
+  ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager, Backbone, Marionette, $, _){
+    Edit.Controller = {
+      editContact: function(id){
+        require(["common/views", "entities/contact"], function(CommonViews){
+          var loadingView = new CommonViews.Loading({
+            title: "Artificial Loading Delay",
+            message: "Data loading is delayed to demonstrate using a loading view."
           });
+          ContactManager.mainRegion.show(loadingView);
 
-          view.on("form:submit", function(data){
-            if(contact.save(data)){
-              ContactManager.trigger("contact:show", contact.get("id"));
+          var fetchingContact = ContactManager.request("contact:entity", id);
+          $.when(fetchingContact).done(function(contact){
+            var view;
+            if(contact !== undefined){
+              view = new View.Contact({
+                model: contact,
+                generateTitle: true
+              });
+
+              view.on("form:submit", function(data){
+                if(contact.save(data)){
+                  ContactManager.trigger("contact:show", contact.get('id'));
+                }
+                else{
+                  view.triggerMethod("form:data:invalid", contact.validationError);
+                }
+              });
             }
             else{
-              view.triggerMethod("form:data:invalid", contact.validationError);
+              view = new ContactManager.ContactsApp.Show.MissingContact();
             }
-          });
-        }
-        else{
-          view = new ContactManager.ContactsApp.Show.MissingContact();
-        }
 
-        ContactManager.mainRegion.show(view);
-      });
-    }
-  };
+            ContactManager.mainRegion.show(view);
+          });
+        });
+      }
+    };
+  });
+
+  return ContactManager.ContactsApp.Edit.Controller;
 });
