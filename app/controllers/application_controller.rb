466ff0cf0854cd0ed084347cb90ca8b8776e2ff3
David Sulc <davidsulc@gmail.com>
2013-11-03 16:16:42 +0100

    get rails to process API values properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,39 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  # rebuild parameters into the structure rails expects them (mainly:
+  # they must be within a containing root attribute, and in snake_case).
+  # Backbone sends them as camelCase and without a root node
+  def process_params(val, root_node)
+    attributes = val.except(:action, :controller, root_node)
+    attributes = sanitize_params(attributes)
+    attributes = deep_snake_case_params(attributes)
+    params = {"#{root_node}" => attributes, "action" => val["action"], "controller" => val["controller"]}
+    puts "rebuilt params: #{params}"
+    params = ActionController::Parameters.new(params)
+  end
+
+  def sanitize_params(params)
+    params.reject{|k,v| ["id"].include? k}
+  end
+
+  # convert all camelCase params (sent by Backbone to the API) to the
+  # snake_case expected by Rails
+  # from http://stackoverflow.com/questions/17240106/what-is-the-best-way-to-convert-all-controller-params-from-camelcase-to-snake-ca
+  def deep_snake_case_params(val)
+    case val
+    when Array
+      val.map {|v| deep_snake_case_params v }
+    when Hash
+      val.keys.each do |k, v = val[k]|
+        val.delete k
+        val[k.underscore] = deep_snake_case_params(v)
+      end
+     val
+    else
+     val
+    end
+  end
 end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -69,6 +69,10 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
+      if parameters=params
+        params = process_params(parameters, "contact")
+        puts "params after processing: #{params}"
+      end
       params.require(:contact).permit(:first_name, :last_name, :phone_number)
     end
 end
