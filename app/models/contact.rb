681bd6a9cf23872697f1f261ad257f4922d9148c
David Sulc <davidsulc@gmail.com>
2013-12-28 13:10:13 +0100

    add server-side validation
    * first name: presence
    * last name: presence, minimal length (2)
    * phone number: unique if present

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ActiveRecord::Base
+  validates :first_name, :presence => true
+  validates :last_name, :length => { :minimum => 2 }
+  validates :phone_number, :uniqueness => true, :allow_blank => true
 end
