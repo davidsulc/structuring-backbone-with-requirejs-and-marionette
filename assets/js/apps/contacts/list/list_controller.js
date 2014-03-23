5e76e5be0e840186f8fa0d16adde76009df68349
David Sulc <davidsulc@gmail.com>
2014-03-23 13:42:51 +0100

    implement contact creation from modal window

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -18,6 +18,41 @@ define(["app", "apps/contacts/list/list_view"], function(ContactManager, View){
               contactsListLayout.contactsRegion.show(contactsListView);
             });
 
+            contactsListPanel.on("contact:new", function(){
+              require(["apps/contacts/new/new_view"], function(NewView){
+                var newContact = ContactManager.request("contact:entity:new");
+
+                var view = new NewView.Contact({
+                  model: newContact
+                });
+
+                view.on("form:submit", function(data){
+                  if(contacts.length > 0){
+                    var highestId = contacts.max(function(c){ return c.id; }).get("id");
+                    data.id = highestId + 1;
+                  }
+                  else{
+                    data.id = 1;
+                  }
+                  if(newContact.save(data)){
+                    contacts.add(newContact);
+                    view.trigger("dialog:close");
+                    var newContactView = contactsListView.children.findByModel(newContact);
+                    // check whether the new contact view is displayed (it could be
+                    // invisible due to the current filter criterion)
+                    if(newContactView){
+                      newContactView.flash("success");
+                    }
+                  }
+                  else{
+                    view.triggerMethod("form:data:invalid", newContact.validationError);
+                  }
+                });
+
+                ContactManager.dialogRegion.show(view);
+              });
+            });
+
             contactsListView.on("itemview:contact:edit", function(childView, args){
               require(["apps/contacts/edit/edit_view"], function(EditView){
                 var model = args.model;
